153faf3ca3db3701a380b65163b5ec9017936cb0
Hideaki Tamai <tamadon3776@gmail.com>
2021-01-18 11:03:18 +0900

    add ios app project

---
+++ esa-iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  esa-iOS
+//
+//  Created by hideaki.tamai on 2021/01/18.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
